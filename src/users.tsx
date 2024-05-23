5553f065ab2cbb0b94c204c66709b45a02214d06
Alpha Olomi <alphaolomi@gmail.com>
2024-05-23 09:36:46 +0300

    Refactor UserList component in users.tsx

--- src/users.tsx
+++ src/users.tsx
@@ -1,17 +1,29 @@
 // in src/users.tsx
-import { List, Datagrid, TextField, EmailField } from "react-admin";
+import { useMediaQuery, Theme } from "@mui/material";
+import { List, SimpleList, Datagrid, TextField, EmailField } from "react-admin";
 
-export const UserList = () => (
-    <List>
-        <Datagrid rowClick="edit">
-            <TextField source="id" />
-            <TextField source="name" />
-            <TextField source="username" />
-            <EmailField source="email" />
-            <TextField source="address.street" />
-            <TextField source="phone" />
-            <TextField source="website" />
-            <TextField source="company.name" />
-        </Datagrid>
-    </List>
-);
+export const UserList = () => {
+    const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
+    return (
+        <List>
+            {isSmall ? (
+                <SimpleList
+                    primaryText={(record) => record.name}
+                    secondaryText={(record) => record.username}
+                    tertiaryText={(record) => record.email}
+                />
+            ) : (
+                <Datagrid rowClick="edit">
+                    <TextField source="id" />
+                    <TextField source="name" />
+                    <TextField source="username" />
+                    <EmailField source="email" />
+                    <TextField source="address.street" />
+                    <TextField source="phone" />
+                    <TextField source="website" />
+                    <TextField source="company.name" />
+                </Datagrid>
+            )}
+        </List>
+    );
+};
