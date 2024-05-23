5ca2b617c93a3d6647ce2a8ba782acb5c6ffaeda
Alpha Olomi <alphaolomi@gmail.com>
2024-05-23 09:36:15 +0300

    Update resource names and add UserList component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
 import {
   Admin,
   Resource,
-  ListGuesser,
+  // ListGuesser,
   EditGuesser,
   ShowGuesser,
 } from "react-admin";
 import { dataProvider } from "./dataProvider";
 import { authProvider } from "./authProvider";
+import { UserList } from "./users";
 
 export const App = () => (
   <Admin dataProvider={dataProvider} authProvider={authProvider}>
-    <Resource
+    {/* <Resource
       name="customers"
       options={{ label: "Customers" }}
       list={ListGuesser}
       edit={EditGuesser}
       show={ShowGuesser}
-    />
+    /> */}
     <Resource
       name="users"
       options={{ label: "Users" }}
-      list={ListGuesser}
+      list={UserList}
       edit={EditGuesser}
       show={ShowGuesser}
     />
-    <Resource
+    {/* <Resource
       name="products"
       options={{ label: "Products" }}
       list={ListGuesser}
       edit={EditGuesser}
       show={ShowGuesser}
-    />
+    /> */}
   </Admin>
 );
